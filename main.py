022fb28ecfd0e0fb0103d2a90ff272c0aa579bdc
Jonas-M-Bruns <github@jonas.ruhr>
2025-10-08 19:36:40 +0200

    Added --verbose flag support

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
-
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -12,11 +12,21 @@ client = genai.Client(api_key=api_key)
 def main():
     if len(sys.argv) <= 1:
         sys.exit(1)
-    response = client.models.generate_content(model="gemini-2.0-flash-001",contents=sys.argv[1])
-    print(response.text.strip())
-    metadata = dict(response.usage_metadata)
-    print(f"Prompt tokens: {metadata["prompt_token_count"]}")
-    print(f"Response tokens: {metadata["candidates_token_count"]}")
+    user_prompt = sys.argv[1]
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages
+    )
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        metadata = dict(response.usage_metadata)
+        print(f"Prompt tokens: {metadata["prompt_token_count"]}")
+        print(f"Response tokens: {metadata["candidates_token_count"]}")
+    
+    print(f"AI Output: {response.text.strip()}")
 
 if __name__ == "__main__":
     main()
